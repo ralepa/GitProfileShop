4be5180645a6dea5f1f099380284a151b2831914
Raphael Pampolha <ralepa@gmail.com>
2016-02-15 02:17:48 -0200

    Adding showdown

--- View/Scripts/Tutorial.jsx
+++ View/Scripts/Tutorial.jsx
@@ -13,8 +13,10 @@
 var CommentList = React.createClass({
     render: function() {
         return (
-          <div className="commentList">
-            Hello, world! I am a CommentList.
+           <div className="commentList">
+            <Comment author="Daniel Lo Nigro">Hello ReactJS.NET World!</Comment>
+            <Comment author="Pete Hunt">This is one comment</Comment>
+            <Comment author="Jordan Walke">This is *another* comment</Comment>
           </div>
       );
     }
@@ -30,6 +32,18 @@ var CommentForm = React.createClass({
     }
 });
 
+var Comment = React.createClass({
+    render: function() {
+        return (
+          <div className="comment">
+            <h2 className="commentAuthor">
+              {this.props.author}
+            </h2>
+        {this.props.children}
+        </div>
+      );
+    }
+});
 
 ReactDOM.render(
   React.createElement(CommentBox, null),
