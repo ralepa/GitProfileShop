e49f31976af5eee737d399ff0533b11751708800
Raphael Pampolha <ralepa@gmail.com>
2016-02-15 02:03:54 -0200

    Initial commit

---
+++ View/App_Start/BundleConfig.cs
@@ -0,0 +1,33 @@
+﻿using System.Web.Optimization;
+
+namespace View
+{
+    public class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+                        "~/lib/jquery/jquery-{version}.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/react").Include(
+                        "~/lib/react/react.min.js",
+                        "~/lib/react/react-dom.min.js",
+                        "~/lib/react/babel.js",
+                        "~/lib/react/gulpfile.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
+                        "~/lib/jquery/jquery.validate*"));
+            
+            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
+                        "~/lib/modernizr/modernizr-*"));
+
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+                      "~/lib/bootstrap/bootstrap.js",
+                      "~/lib/respond/respond.js"));
+
+            bundles.Add(new StyleBundle("~/style/css").Include(
+                      "~/lib/bootstrap/bootstrap.css",
+                      "~/style/main.css"));
+        }
+    }
+}
---
+++ View/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace View.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Cart()
+        {
+            return View();
+        }
+
+        public ActionResult ReactTest()
+        {
+            return View();
+        }
+
+    }
+}
